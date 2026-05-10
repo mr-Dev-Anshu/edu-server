5630d6a2cad21bd5860ad18d188e6832b47b7418
knightwolf01 <sanukumarsingh387@gmail.com>
2026-05-11 03:18:01 +0530

    created the org admin

--- config/masterPermission.js
+++ config/masterPermission.js
@@ -1,40 +1,5 @@
 export const ADMIN_PERMISSIONS = [
-  "388b816d-73c8-42ec-9499-5befdd38e646", "ce3b9446-a3ca-4b7f-a0dd-854762fe2936",
-  "a750a7df-c1ea-4658-9421-c7fb8787ef5a", "43814e52-08f0-4038-b480-bffda06386f5",
-  "6c654c89-a41a-4af7-a741-1934d8ecb74d", "a97c79ca-5c6f-4d92-9dab-a7b121eafab2",
-  "acbb56ee-df26-4422-909e-569ec3b19ca8", "093be964-70a5-4d12-a51d-f3f25f1dd2d1",
-  "5103e362-3e2f-4289-86f2-6cf7f9020ced", "b61f9b6f-162d-481c-a1ad-016656881100",
-  "e0170dfe-7901-48c0-8fe4-a314f1f1ada8", "a1a9549f-2d11-4916-85a5-87f4bb019c9f",
-  "38b1ae16-5268-4d18-89fd-977c89147c05", "5bb30b18-4d14-4844-a5d4-643cb93d45f2",
-  "c55f0a1c-60ac-4488-a6a3-ea065626e09b", "5349b9cf-e2f6-4c6b-99fc-a309560e0597",
-  "2d6b15c5-f55f-4767-80a5-532a815a5aeb", "f4c9d4ac-2364-4d5f-8063-80e162b2d407",
-  "fb9c8d69-3c42-4c34-83ec-09f4a7db1579", "75047d84-6b70-4b04-95a3-55e54e49662a",
-  "f3c24f31-3655-4fa9-b793-8c08b6394c8a", "6cfe9923-1547-445d-b86c-f487df6fac1b",
-  "42615c42-17c5-4d4c-a0e1-605e8b6287d6", "0ec6d1d1-e078-4eca-9f2a-70a28ebc4b4b",
-  "0702613d-2066-4623-8822-c044cee88f2e", "b1ebbe15-be21-43a4-a5e9-9d3f5ac0fe8d",
-  "ad342757-12ba-49f9-bd5e-9a3b7ee6ad8e", "180a3fef-6b10-41b5-a231-4fe5f0b0ad4a",
-  "317c6866-2476-48a6-894e-bb9e2822a949", "52b15983-cdb9-40be-8b11-6a0881601e36",
-  "9fc6e4aa-e556-43d3-99c3-3bd3ba24abe0", "f497d94f-2e39-4d95-a71d-4e9909340fc1",
-  "7f77bf3e-b0a2-433e-beb7-a03ecd3a40d2", "abf41646-c6fd-4e38-b7e1-bf4835086331",
-  "7e999423-1350-49f5-9fa1-4c6b6c06fde4", "63cea48c-b37a-4f7b-bf2a-a5306544fadc",
-  "b613dbe1-39ad-4789-a7e3-b83ba66f0ae6", "943d723f-6e42-4d63-8df6-a68c8b6e10a6",
-  "5a4ec8f5-388a-4703-88ed-44e2d2693456", "def62c30-bab6-4499-acfe-6f0f9cbe203a",
-  "7d027500-a04f-47ce-a505-773a80023e17", "5b97e53a-6f8f-4350-aeca-c92b0bf24b40",
-  "6b4f83bc-45dd-4bbe-9b60-1a6a262cb8a0", "64c4bf11-8057-4ecc-b3cb-6cad38b9ae86",
-  "505e9106-6514-41ba-b806-8f0f327f33fc", "810e7fd8-2fa7-42e9-b08f-0961de95e743",
-  "99a931bb-e0a2-48ab-b1b3-c7ba18c1131e", "fc30be4c-f7c7-48c7-a9b1-6e74b1614211",
-  "e9750422-b91b-48aa-82ce-e5a8bb07367c", "6034f8a2-2f20-4160-8a13-4f35b6df44d8",
-  "22ca2a93-afd5-4a82-85b4-e91a664d40b0", "abf175c8-9fdc-4a75-914b-5c433ea79202",
-  "c6ed958a-d322-43e3-bee3-415e54478e85", "434b1d1d-266b-4799-a164-523b998d8e79",
-  "c35b5916-26ea-400f-b656-6866f3e3de38", "d22c5090-dc9d-4acd-9e53-30e1b85355bf",
-  "a07f99e8-67aa-48a6-9443-d6b0ac117081", "9a07b60f-9554-4115-a2ea-a901e0cf590d",
-  "bb9d3c31-b740-4a88-b488-c03c5e809b6c", "7bda3fd1-945e-4200-8f7e-c792139e15b0",
-  "34f294be-a164-4391-8239-e97bb6ff702c", "b1adfc70-1c10-4a9b-b69e-9f643c1ff6e5",
-  "adff878e-8eef-4c21-adfb-5bb806d371e3", "601720cc-95cd-4f51-a452-130584b2c888",
-  "3c15f1ca-d692-41fe-a432-c3f34e9c29f2", "7c46cbfc-40e3-400e-9ce4-5692f3f5dbf2",
-  "e21bb7c9-b66a-400f-872d-a9232a52d74a", "80b18a1f-ada3-4855-93d2-9b6ce337d34a",
-  "5c5faf4c-2a16-4643-8d57-dfe32a31ab9a", "a5e3cc76-47e7-4474-b3a9-4ce29c213e06",
-  "2c28ae77-7b6c-494f-82f1-f61609dbd2ca", "6d7e34a9-1f35-419c-8302-25d9ee710c97"
+  "5baa08a7-9317-44b9-8ce2-ba5ab941f095", "3e7e57ee-f340-4b6c-a6b1-0a280790bc00", "ff64948e-29c4-40aa-994c-20b88a02fe20", "9630b91a-5bcd-4c91-80e9-d8b0f61644d7", "f4b7b6a6-07e3-4257-b538-6fd87f234997", "77c2b4d4-a842-4bde-8057-ae80b0da62a0", "4d98e864-83a3-4f93-bf9e-1f8b93e1b400", "e0638adc-67a7-4aee-817b-0e0ebb28a379", "f92efebd-38c6-4070-ae84-965cece295fb", "ffe3add6-61af-4aa7-a4ee-701b03dc13b2", "77907469-5c96-4d04-b9de-64ede64ffdc6","4cbc1225-cd90-45c0-b5a0-d0d89e05f977", "25ad33b9-3b4f-454f-a3ec-b8cc1276c803", "3dfb37ea-ff42-4230-b9f7-dcedc7567369", "f92efebd-38c6-4070-ae84-965cece295fb", "5c0b5729-92bd-4078-9097-66a463a406e2", "55e7697b-2f75-4ff0-8cbe-b3f8736a7d26", "0459dfb0-0c0f-40ef-ae36-7ba6d7542f29", "db549f10-0b0f-4dc4-bfb9-e71b1de6f477", "ce89b133-487f-416f-9f1c-c1d29a300f52", "a3b6cd72-956a-4474-9e9d-22f24fa857ad","9630b91a-5bcd-4c91-80e9-d8b0f61644d7", "e0638adc-67a7-4aee-817b-0e0ebb28a379", "f92efebd-38c6-4070-ae84-965cece295fb", "99f16737-d8c1-4557-b17d-0f03516bd578", "a3b6cd72-956a-4474-9e9d-22f24fa857ad", "d2044cea-ff5d-4b97-a35a-07f88d13f45f","29806768-db39-485c-a839-6beb0a9b8aae", "9630b91a-5bcd-4c91-80e9-d8b0f61644d7", "e0638adc-67a7-4aee-817b-0e0ebb28a379", "f92efebd-38c6-4070-ae84-965cece295fb", "2eb74baf-e0d6-4ae1-bb12-0cd09713cd50", "e59d138a-83b7-407e-8f82-7ce07ca5632b", "f1fd0fd7-42b7-4408-b6a8-515835a29b32", "1b1da751-e378-4d3b-9a9c-91148e980a8a", "a5abd595-e7ad-41b5-83f6-f9fa9cb0753f", "d0ee78dd-1c2d-400f-8d6c-9239edd6db6a", "e7116083-1d40-4c85-b7fb-c3d357a59126","3e7e57ee-f340-4b6c-a6b1-0a280790bc00", "25ad33b9-3b4f-454f-a3ec-b8cc1276c803", "4d98e864-83a3-4f93-bf9e-1f8b93e1b400", "f92efebd-38c6-4070-ae84-965cece295fb"
 ];
 
 export const ROLE_MASTER_CONFIG = {
@@ -43,76 +8,31 @@ export const ROLE_MASTER_CONFIG = {
     roleType: "staff",
     hierarchyLevel: 5,
     permissions: [
-      "ce3b9446-a3ca-4b7f-a0dd-854762fe2936", // read:students
-      "38b1ae16-5268-4d18-89fd-977c89147c05", // create:attendance
-      "5bb30b18-4d14-4844-a5d4-643cb93d45f2", // read:attendance
-      "c55f0a1c-60ac-4488-a6a3-ea065626e09b", // update:attendance
-      "317c6866-2476-48a6-894e-bb9e2822a949", // create:exams
-      "52b15983-cdb9-40be-8b11-6a0881601e36", // read:exams
-      "9fc6e4aa-e556-43d3-99c3-3bd3ba24abe0", // update:exams
-      "b1ebbe15-be21-43a4-a5e9-9d3f5ac0fe8d", // read:subjects
-      "9a07b60f-9554-4115-a2ea-a901e0cf590d", // read:teacher_assignment
-      "7d027500-a04f-47ce-a505-773a80023e17", // create:notices
-      "5b97e53a-6f8f-4350-aeca-c92b0bf24b40"  // read:notices
+      "5baa08a7-9317-44b9-8ce2-ba5ab941f095", "3e7e57ee-f340-4b6c-a6b1-0a280790bc00", "ff64948e-29c4-40aa-994c-20b88a02fe20", "9630b91a-5bcd-4c91-80e9-d8b0f61644d7", "f4b7b6a6-07e3-4257-b538-6fd87f234997", "77c2b4d4-a842-4bde-8057-ae80b0da62a0", "4d98e864-83a3-4f93-bf9e-1f8b93e1b400", "e0638adc-67a7-4aee-817b-0e0ebb28a379", "f92efebd-38c6-4070-ae84-965cece295fb", "ffe3add6-61af-4aa7-a4ee-701b03dc13b2", "77907469-5c96-4d04-b9de-64ede64ffdc6"
     ]
   },
   accountant: {
     name: "Accountant",
     roleType: "staff",
     hierarchyLevel: 6,
-    permissions: [
-      "5103e362-3e2f-4289-86f2-6cf7f9020ced", // create:fees
-      "b61f9b6f-162d-481c-a1ad-016656881100", // read:fees
-      "e0170dfe-7901-48c0-8fe4-a314f1f1ada8", // update:fees
-      "7f77bf3e-b0a2-433e-beb7-a03ecd3a40d2", // create:invoices
-      "abf41646-c6fd-4e38-b7e1-bf4835086331", // read:invoices
-      "7e999423-1350-49f5-9fa1-4c6b6c06fde4", // delete:invoices
-      "b613dbe1-39ad-4789-a7e3-b83ba66f0ae6", // read:payroll
-      "5a4ec8f5-388a-4703-88ed-44e2d2693456", // update:payroll
-      "e9750422-b91b-48aa-82ce-e5a8bb07367c", // read:infrastructure
-      "5b97e53a-6f8f-4350-aeca-c92b0bf24b40"  // read:notices
-    ]
+    permissions: ["4cbc1225-cd90-45c0-b5a0-d0d89e05f977", "25ad33b9-3b4f-454f-a3ec-b8cc1276c803", "3dfb37ea-ff42-4230-b9f7-dcedc7567369", "f92efebd-38c6-4070-ae84-965cece295fb", "5c0b5729-92bd-4078-9097-66a463a406e2", "55e7697b-2f75-4ff0-8cbe-b3f8736a7d26", "0459dfb0-0c0f-40ef-ae36-7ba6d7542f29", "db549f10-0b0f-4dc4-bfb9-e71b1de6f477", "ce89b133-487f-416f-9f1c-c1d29a300f52", "a3b6cd72-956a-4474-9e9d-22f24fa857ad"]
   },
   librarian: {
     name: "Librarian",
     roleType: "staff",
     hierarchyLevel: 7,
-    permissions: [
-      "e9750422-b91b-48aa-82ce-e5a8bb07367c", // read:infrastructure
-      "6034f8a2-2f20-4160-8a13-4f35b6df44d8", // create:infrastructure
-      "22ca2a93-afd5-4a82-85b4-e91a664d40b0", // update:infrastructure
-      "7d027500-a04f-47ce-a505-773a80023e17", // create:notices
-      "5b97e53a-6f8f-4350-aeca-c92b0bf24b40", // read:notices
-      "ce3b9446-a3ca-4b7f-a0dd-854762fe2936"  // read:students
-    ]
+    permissions: ["9630b91a-5bcd-4c91-80e9-d8b0f61644d7", "e0638adc-67a7-4aee-817b-0e0ebb28a379", "f92efebd-38c6-4070-ae84-965cece295fb", "99f16737-d8c1-4557-b17d-0f03516bd578", "a3b6cd72-956a-4474-9e9d-22f24fa857ad", "d2044cea-ff5d-4b97-a35a-07f88d13f45f"]
   },
   "admission-head": {
     name: "Admission Head",
     roleType: "staff",
     hierarchyLevel: 4,
-    permissions: [
-      "505e9106-6514-41ba-b806-8f0f327f33fc", // create:admission_lead
-      "810e7fd8-2fa7-42e9-b08f-0961de95e743", // read:admission_lead
-      "99a931bb-e0a2-48ab-b1b3-c7ba18c1131e", // update:admission_lead
-      "fc30be4c-f7c7-48c7-a9b1-6e74b1614211", // delete:admission_lead
-      "388b816d-73c8-42ec-9499-5befdd38e646", // create:students
-      "ce3b9446-a3ca-4b7f-a0dd-854762fe2936", // read:students
-      "ba6b3877-3153-47c3-ab3c-db328dc79842", // create:guardian
-      "e167892e-467c-44bd-9376-aa612600e8f1", // read:guardian
-      "0a1e70da-7545-4e2a-80c3-badf4b0b65f3", // update:guardian
-      "7d027500-a04f-47ce-a505-773a80023e17", // create:notices
-      "5b97e53a-6f8f-4350-aeca-c92b0bf24b40"  // read:notices
-    ]
+    permissions: ["29806768-db39-485c-a839-6beb0a9b8aae", "9630b91a-5bcd-4c91-80e9-d8b0f61644d7", "e0638adc-67a7-4aee-817b-0e0ebb28a379", "f92efebd-38c6-4070-ae84-965cece295fb", "2eb74baf-e0d6-4ae1-bb12-0cd09713cd50", "e59d138a-83b7-407e-8f82-7ce07ca5632b", "f1fd0fd7-42b7-4408-b6a8-515835a29b32", "1b1da751-e378-4d3b-9a9c-91148e980a8a", "a5abd595-e7ad-41b5-83f6-f9fa9cb0753f", "d0ee78dd-1c2d-400f-8d6c-9239edd6db6a", "e7116083-1d40-4c85-b7fb-c3d357a59126"]
   },
   student: {
     name: "Student",
     roleType: "portal",
     hierarchyLevel: 10,
-    permissions: [
-      "5bb30b18-4d14-4844-a5d4-643cb93d45f2", // read:attendance
-      "52b15983-cdb9-40be-8b11-6a0881601e36", // read:exams
-      "5b97e53a-6f8f-4350-aeca-c92b0bf24b40", // read:notices
-      "b61f9b6f-162d-481c-a1ad-016656881100"  // read:fees
-    ]
+    permissions: ["3e7e57ee-f340-4b6c-a6b1-0a280790bc00", "25ad33b9-3b4f-454f-a3ec-b8cc1276c803", "4d98e864-83a3-4f93-bf9e-1f8b93e1b400", "f92efebd-38c6-4070-ae84-965cece295fb"]
   }
 };
